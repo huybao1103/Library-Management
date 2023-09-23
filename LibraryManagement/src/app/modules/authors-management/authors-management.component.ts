48c652dbe627e5896dc2aa520675aaebac4ca62c
pinotc <91887348+pinotc@users.noreply.github.com>
2023-09-23 12:15:16 +0700

    authors-management.component.ts

--- LibraryManagement/src/app/modules/authors-management/authors-management.component.ts
+++ LibraryManagement/src/app/modules/authors-management/authors-management.component.ts
@@ -1,46 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { IAuthor } from 'src/app/models/author.model';
-import { HttpService } from 'src/app/services/http-service.service';
-import { AuthorService } from './service/author.service';
-import { Observable } from 'rxjs';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
-  selector: 'app-authors-management',
-  templateUrl: './authors-management.component.html',
-  styleUrls: ['./authors-management.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AuthorsManagementComponent implements OnInit {
-  authorData: IAuthor[] = [];
-  author$?: Observable<IAuthor[] | null>;
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(DataTableDirective, { static: false })
+  private datatableElement!: DataTableDirective;
 
-  constructor(
-    private httpService: HttpService,
-    private route: Router,
-    private authorService: AuthorService
-  ) {
+  title = 'AuthorManager';
+  dtOptions: DataTables.Settings = {};
+  authors = [
+    {
+      id: 1,
+      name: 'Nguyễn Văn A',
+      email: 'nvan@example.com',
+      phone: '0123456789'
+    }
+  ];
+  author = {
+    id: undefined as number | undefined,
+    name: '',
+    email: '',
+    phone: ''
+  };
+  searchQuery = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      lengthMenu: [5, 10, 25],
+      processing: true
+    };
   }
 
-  ngOnInit(): void {
-    this.getData();
+  ngAfterViewInit(): void {
+    this.refreshDataTable();
   }
 
-  getData() {
-    // /api/Authors
-    // this.authorService.getAll().subscribe({
-    //   next: (resp) => {
-    //     if(resp)
-    //       this.authorData = resp;
-
-    //     console.log(this.authorData);
-    //   }
-    // })
-
-    this.author$ = this.authorService.getAll();
+  refreshDataTable(): void {
+    if (this.datatableElement && this.datatableElement.dtInstance) {
+      this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        this.dtOptions = Object.assign({}, this.dtOptions);
+      });
+    }
   }
 
-  edit(id?: string) {
-    console.log('selected author id ' + id);
-    this.route.navigate([{ outlets: { modal: ['author', 'edit', id] } }]);
+  addAuthor(): void {
+    // Add your logic for adding an author here
+  }
+
+  deleteAuthor(index: number): void {
+    this.authors.splice(index, 1);
+    this.refreshDataTable();
+  }
+
+  editAuthor(index: number): void {
+    const editedAuthor = Object.assign({}, this.authors[index]);
+    if (editedAuthor.id === undefined || editedAuthor.id === null) {
+      editedAuthor.id = null as unknown as number;
+    }
+    this.author = editedAuthor;
+    this.authors.splice(index, 1);
+    this.refreshDataTable();
+  }
+
+  get filteredAuthors(): any[] {
+    // Filter authors based on the search query
+    if (this.searchQuery) {
+      const searchTerm = this.searchQuery.toLowerCase();
+      return this.authors.filter(author => {
+        return author.name.toLowerCase().includes(searchTerm) ||
+               author.email.toLowerCase().includes(searchTerm) ||
+               author.phone.toLowerCase().includes(searchTerm);
+      });
+    } else {
+      return this.authors;
+    }
   }
 }
