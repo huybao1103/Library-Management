9335fa369d82c4def51a99181bef62e0e8022526
huybao1103 <l.q.h.bao@gmail.com>
2023-09-23 12:57:19 +0700

    Fix cannot save publisher

--- LibraryAPI/Controllers/PublishersController.cs
+++ LibraryAPI/Controllers/PublishersController.cs
@@ -69,21 +69,20 @@ namespace LibraryAPI.Controllers
 
             RequestSavePublisherValidate(publisherRequestModel);
 
-            PublisherModel? publisherModel;
-            if (publisherRequestModel.Id == Guid.Empty)
+            Publisher? publisher;
+            if (!publisherRequestModel.Id.HasValue)
             {
-                var newPublisher = _mapper.Map<Publisher>(publisherRequestModel);
-                _context.Publishers.Add(newPublisher);
-                publisherModel = _mapper.Map<PublisherModel>(newPublisher);
+                publisher = _mapper.Map<Publisher>(publisherRequestModel);
+                _context.Publishers.Add(publisher);
             }
             else
             {
-                var publisher = await _context.Publishers.FindAsync(publisherRequestModel.Id);
+                publisher = await _context.Publishers.FindAsync(publisherRequestModel.Id);
                 publisher = _mapper.Map(publisherRequestModel, publisher);
-                publisherModel = _mapper.Map<PublisherModel>(publisher);
             }
 
-            return CreatedAtAction("GetPublisher", new { id = publisherModel.Id }, publisherModel);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPublisher", new { id = publisher.Id }, _mapper.Map<PublisherModel>(publisher));
         }
 
         // DELETE: api/Publishers/5
