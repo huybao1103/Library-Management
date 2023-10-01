82d0913f439df27fea9ef102e0ce56fc1ad1e5c3
huybao1103 <l.q.h.bao@gmail.com>
2023-10-01 19:56:32 +0700

    LIB-35: delete file

---
+++ LibraryAPI/ViewModels/LibraryCard/StudentImageModel.cs
@@ -0,0 +1,19 @@
+﻿using LibraryAPI.ViewModels.File;
+
+namespace LibraryAPI.ViewModels.LibraryCard
+{
+    public class StudentImageModel
+    {
+        public Guid? Id { get; set; }
+
+        public string? FilePath { get; set; }
+
+        public Guid? FileId { get; set; }
+
+        public Guid? LibraryCardId { get; set; }
+
+        public string? Base64 { get; set; }
+
+        public virtual UploadFileModel? File { get; set; }
+    }
+}
