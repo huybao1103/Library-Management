4b93de8832bdee034dec600bfe2652f55473281c
Huytx203 <huy05072003huy@gmail.com>
2023-11-04 10:35:00 +0700

    add account

--- LibraryManagement/src/app/app.module.ts
+++ LibraryManagement/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LoginComponent } from './login-register/login/login.component';
 import { RegisterComponent } from './login-register/register/register.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- LibraryManagement/src/app/modules/account-management/reader/reader-account-detail/reader-account-detail.component.ts
+++ LibraryManagement/src/app/modules/account-management/reader/reader-account-detail/reader-account-detail.component.ts
@@ -1,53 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Route } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { IDialogType } from 'src/app/models/modal/dialog';
+import { IReaderAccount } from 'src/app/models/reader-account.model';
+import { AccountDetailFields } from './reader-account.form';
+import { HttpErrorResponse } from '@angular/common/http';
+import { uniqueId } from 'lodash';
+import { HttpService } from 'src/app/services/http-service.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { MessageType } from 'src/app/enums/toast-message.enum';
 
 @Component({
   selector: 'app-reader-account-detail',
   templateUrl: './reader-account-detail.component.html',
   styleUrls: ['./reader-account-detail.component.css']
 })
-export class ReaderAccountDetailComponent implements IDialogType {
-  uniqueId: string = '';
-  width?: string | undefined;
-  height?: string | undefined;
-  size?: 'sm' | 'lg' | 'xl' | undefined;
-
-  title: string = "";
-  id: string = "";
-  
-  fields: FormlyFieldConfig[] = [];
+export class ReaderAccountDetailComponent implements IDialogType, OnInit {
+  uniqueId: string = uniqueId('account-info');
+  title: string = '';
+  fields: FormlyFieldConfig[] = []; // abcxyz
   form = new FormGroup({});
   options: FormlyFormOptions = {
     formState: {
       optionList: {
-         
+      
       }
     }
   };
+
+  data: IReaderAccount = {
+    name: '',
+    mail: '',
+    pass: '',
+    status: '',
+    getAccountById: function (id: string): unknown {
+      throw new Error('Function not implemented.');
+    },
+    save: function (data: IReaderAccount): unknown {
+      throw new Error('Function not implemented.');
+    }
+  }  
   
-  data: any; // Tạo model mới rồi sửa lại kiểu dữ liệu
+   publisher: IReaderAccount[] = [];
+
 
   constructor(
     private modal: NgbActiveModal,
+    private toastService: ToastService,
+    private httpService: HttpService,
+    private accountService: AccountService,
   ) {
+  }
+
+  dialogInit(para: { id: string }): void {
+    this.title = "Add Account";
+    if (para.id) {
+      this.title = "Edit Account Information";
+      this.getAccountById(para.id);
+      console.log(para.id);
+    } else {
+      this.fields = AccountDetailFields();
+    }
+  }
+
+  ngOnInit(): void {
 
   }
 
-  dialogInit(para: any, routeConfig?: Route | undefined): void {
-    this.id = para.id;
-    
-    this.title = `${this.id ? "Edit" : "Add"} Account`;
+  getAccountById(id: string) {
+    this.accountService.getAccountById(id).subscribe({
+      next: (res: IReaderAccount) => {
+        if (res)
+          this.data = res;
+          this.fields = AccountDetailFields();
+      }
+    })
+  }
+
+  loadData(id: string) {
+    this.accountService.getPublisherById(id).subscribe({
+      next: (res: IReaderAccount) => {
+        if(res)
+          this.data = res;
+        this.fields = AccountDetailFields();
+      } 
+    })
   }
 
   submit() {
-    
+    this.accountService.save(this.data).subscribe({
+      next: (resp: any) => {
+        this.toastService.show(MessageType.success, 'Account info save success');
+        this.close();
+      },
+      error: (err: HttpErrorResponse) => {
+        this.toastService.show(MessageType.error, err.error?.detail);
+      }
+    })
   }
 
-  close() {
-    this.modal.close();
+
+
+  close() { this.modal.close() }
+
+  private addPublisherConfirmed() {
+    this.accountService.save(this.data).subscribe({
+      next: (resp: any) => {
+        console.log(resp);
+        this.toastService.show(MessageType.success, 'Account info save success');
+
+        this.close();
+      },
+      error: (err: HttpErrorResponse) => {
+        this.toastService.show(MessageType.error, err.error?.detail);
+      }
+    })
   }
 }
